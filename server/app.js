778962282c40d9e435337d03c11b670f71db244e
Your Name <you@example.com>
2023-12-06 14:03:28 -0800

    first commit

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use('/api', router);
 
 // After Routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 /**
